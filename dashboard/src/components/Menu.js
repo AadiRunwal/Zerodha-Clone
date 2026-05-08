486299a65bed7fbbb8406c13a4f07ed03c525377
Aadi Runwal <aadirunwal537@gmail.com>
2026-05-09 00:16:05 +0530

    Update Menu.js

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -23,7 +23,7 @@ const Menu = ({username}) => {
   const handleLogout = ()=>{
     console.log(cookies);
     removeCookies("token");
-    window.location.href="https://zerodha-clone-webapp.netlify.app/login";
+    window.location.href="https://zerodha-clone-uqzt.onrender.com/login";
   }
 
   return (
